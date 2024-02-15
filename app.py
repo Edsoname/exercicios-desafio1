4d0915807c3b25634b9bbdc13009c5d4e0d61a57
codigo de automação teste 1 <edson451.ea@gmail.com>
2024-02-15 17:51:52 -0300

    projeto pyhon

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import pyautogui
 
-pyautogui.rightClick(728,278,duration=2)
-pyautogui.move(200,370,duration=2)
-pyautogui.move(200,0,duration=2)
-pyautogui.click(4)
+pyautogui.rightClick(116,740,duration=1)
+pyautogui.click()
+pyautogui.move(0,-250,duration=1)
+pyautogui.click()
+pyautogui.move(232,-410,duration=2)
+pyautogui.click()
+pyautogui.move(-110,0,duration=2)
+pyautogui.click()
+pyautogui.move(50,150,duration=2)
+pyautogui.click()
+pyautogui.move(350,-45,duration=2)
+pyautogui.click()
+pyautogui.move(65,330,duration=2)
+pyautogui.click()
+pyautogui.move(520,-440,duration=1)
+pyautogui.click()
+
+
+
+
